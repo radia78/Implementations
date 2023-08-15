08ae4029d6673d83c8be2027d09b615734e0b0d4
Radi Akbar <radia@Radis-MacBook-Pro.local>
2023-08-14 22:38:05 -0400

    Pushing new files

---
+++ week_4/train.py
@@ -0,0 +1,58 @@
+from model import Diffusion
+import os
+import torch
+from tqdm import tqdm
+from unet import UNet
+import torch.nn as nn
+import torch.optim as optim
+import logging
+from torch.utils.tensorboard import SummaryWriter 
+from utils import plot_images, save_images, setup_logging, get_data
+
+def train(args):
+    setup_logging(args.run_name)
+    device = args.device
+    dataloader = get_data(args)
+    model = UNet().to(device)
+    optimizer = optim.AdamW(model.parameters(), lr=args.lr)
+    mse = nn.MSELoss()
+    diffusion = Diffusion(img_size=args.img_size, device=device)
+    logger = SummaryWriter(os.path.join("runs", args.run_name))
+    l = len(dataloader)
+
+    for epoch in range(args.epoch):
+        logging.info(f"Starting epoch {epoch}:")
+        pbar = tqdm(dataloader)
+        for i, (images, _) in enumerate(pbar):
+            images = images.to(device)
+            t = diffusion.sample_timesteps(images.shape[0]).to(device)
+            x_t, noise = diffusion.forward_process(images, t)
+            predicted_noise = model(x_t, t)
+            loss = mse(noise, predicted_noise)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            pbar.set_postfix(MSE=loss.item())
+            logger.add_scalar("MSE", loss.item(), global_step=epoch * l + i)
+        
+        sampled_images = diffusion.sample(model, n=images.shape[0])
+        save_images(sampled_images, os.path.join("results", args.run_name, f"{epoch}.jpg"))
+        torch.save(model.state_dict(), os.path.join("models", args.run_name, f"ckpt.pt"))
+
+def launch():
+    import argparse
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    args.run_name = "DDPM_Uncoditional"
+    args.epochs = 500
+    args.batch_size = 12
+    args.image_size = 64
+    args.device = "cuda"
+    args.lr = 3e-4
+    train(args)
+
+if __name__ == "__main__":
+    launch()
+    
